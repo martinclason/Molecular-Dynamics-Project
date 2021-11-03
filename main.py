7d7630017c5e09506aef9d05bb6aaf43fe282a1b
danle402 <danle402@hunn2-114.ad.liu.se>
2021-11-03 09:31:13 +0100

    Added Parser arguments Added Parser so user can choose if to use asap or not with the flags --asap or --no-asap.

--- main.py
+++ main.py
@@ -4,9 +4,18 @@ from ase.lattice.cubic import FaceCenteredCubic
 from ase.md.velocitydistribution import MaxwellBoltzmannDistribution
 from ase.md.verlet import VelocityVerlet
 from ase import units
+import argparse
+
+
+# Adds parser so user can choose if to use asap or not with flags from terminal
+parser = argparse.ArgumentParser()
+parser.add_argument('--asap', dest='asap', action='store_true')
+parser.add_argument('--no-asap', dest='asap', action='store_false')
+parser.set_defaults(feature=True)
+args = parser.parse_args()
 
 # Use Asap for a huge performance increase if it is installed
-use_asap = True
+use_asap = args.asap
 
 if use_asap:
     from asap3 import EMT
