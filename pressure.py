edbffaa02da265c56ef754675bf5e54c5a68ce2c
danst852 <danst852@tlvm-4-2-1.ad.liu.se>
2021-11-12 11:22:12 +0100

    Volume, Kinetic energy and Position extractable

--- pressure.py
+++ pressure.py
@@ -4,6 +4,8 @@ from ase.md.verlet import VelocityVerlet
 from ase import units
 from ase.visualize import view
 from asap3 import Trajectory
+#from asap3 import LennardJones
+#from xtb.ase.calculator import XTB
 #from ase.io import read
 
 atoms = FaceCenteredCubic(
@@ -34,6 +36,10 @@ else:
 
 # Describe the interatomic interactions with the Effective Medium Theory
 atoms.calc = EMT()
+#atoms.calc = LennardJones("cu", "1", "1")
+#atoms.calc = XTB(method="GFN2-xTB")
+
+
 
     # Set the momenta corresponding to T=300K
 MaxwellBoltzmannDistribution(atoms, temperature_K=3000)
@@ -44,11 +50,23 @@ dyn = VelocityVerlet(atoms, 5 * units.fs)  # 5 fs time step.
 
 traj = Trajectory("cu.traj", "w", atoms)
 dyn.attach(traj.write, interval=10)
-dyn.run(1000)
+dyn.run(100)
 
 traj.close()
 traj_read = Trajectory("cu.traj")
-###atoms_volume_final = traj_read[0].get_volume()[0]
-###print("Final volume is: " + str(atoms_volume_final))
-print("Final volume is: " +  str(traj_read[10].get_volume()))
-print("Final temperature is: " +  str(traj_read[10].get_temperature()))
+
+atoms_volume_final = traj_read[10].get_volume()
+
+
+#print ("atoms_volume_final = " + str(atoms_volume_final))
+print("Final volume is: " + str(atoms_volume_final))
+#print("Final volume is: " +  str(traj_read[10].get_volume()))
+#print("Final temperature is: " +  str(traj_read[0].get_temperature()))
+
+print("Initial position of first atom: " + str(traj_read[0].get_positions()[0]))
+print("Initial position of second atom: " + str(traj_read[0].get_positions()[1]))
+print("Second position of first atom: " + str(traj_read[1].get_positions()[0]))
+print("Kinetic energy after 10 timesteps: " + str(traj_read[1].get_kinetic_energy()))
+print("Kinetic energy after 20 timesteps: " + str(traj_read[2].get_kinetic_energy()))
+
+print("Initial forces on first atom: " + str(traj_read[1].get_forces(apply_constraint = False)[1]))
