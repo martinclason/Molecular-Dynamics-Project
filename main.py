775f8238e6b726a126391d2cdde13ee1f70208a2
danle402 <danle402@tlvm-4-1-1.ad.liu.se>
2021-11-17 15:18:17 +0100

    Fixed MSD calculation MSD calculation implemented correctly. Fixed the issue where the atoms move from their starting positions.

--- main.py
+++ main.py
@@ -8,8 +8,9 @@ from ase.lattice.triclinic import Triclinic
 from ase.lattice.hexagonal import Hexagonal, HexagonalClosedPacked, Graphite
 
 import matplotlib.pyplot as plt
+import math
 
-from ase.md.velocitydistribution import MaxwellBoltzmannDistribution
+from ase.md.velocitydistribution import (MaxwellBoltzmannDistribution,Stationary,ZeroRotation)
 from ase.md.verlet import VelocityVerlet
 from asap3 import Trajectory
 from ase import units
@@ -65,6 +66,8 @@ def MD():
 
     # Set the momenta corresponding to T=300K
     MaxwellBoltzmannDistribution(atoms, temperature_K=parsed_config_file["temperature_K"])
+    Stationary(atoms)
+    ZeroRotation(atoms)
     # We want to run MD with constant energy using the VelocityVerlet algorithm.
     dyn = VelocityVerlet(atoms, 5 * units.fs)  # 5 fs time step.
     if parsed_config_file["make_traj"]:
@@ -78,6 +81,7 @@ def MD():
         print('Energy per atom: Epot = %.3feV  Ekin = %.3feV (T=%3.0fK)  '
               'Etot = %.3feV' % (epot, ekin, ekin / (1.5 * units.kB), epot + ekin))
 
+#Calculates MSD for one time step from .traj file
     def MSD(t,atom_list):
         r0 = atom_list[0].get_positions()
         rt = atom_list[t].get_positions()
@@ -85,9 +89,10 @@ def MD():
         diff= rt-r0
         squareddiff = diff**2
         summ = sum(sum(squareddiff))
-        return (1/N)*summ
-
-
+        normsum = (1/N) * summ
+        #return math.sqrt(normsum[0]**2 + normsum[1]**2 + normsum[2]**2)
+        return normsum
+#Calculates MSD for all the time steps and plots them
     def MSD_plot(time,atom_list):
         MSD_data = []
         for t in range(time):
@@ -101,7 +106,7 @@ def MD():
     # Now run the dynamics
     dyn.attach(printenergy, interval=10)
     printenergy()
-    dyn.run(500)
+    dyn.run(200)
     if parsed_config_file["make_traj"]:
         traj.close()
         traj_read = Trajectory(parsed_config_file["symbol"]+".traj")
