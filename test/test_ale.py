203af5afe7c6e563975c750aaf17d050b0f139c2
Martin Clason <martin@clason.se>
2021-11-30 14:04:02 +0100

    Adds test for that small sim can be run with ale

---
+++ test/test_ale.py
@@ -0,0 +1,7 @@
+import subprocess
+
+def test_ale_small_simulation():
+    try:
+        process = subprocess.run("ale config_small_test.yaml", shell=True, check=True)
+    except:
+        assert False, "ale couldn't run"
