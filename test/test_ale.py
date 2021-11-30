abf836c1322e626427c9977484dcf6429d01ebdd
Martin Clason <martin@clason.se>
2021-11-30 14:11:19 +0100

    Full path to ale in test

--- test/test_ale.py
+++ test/test_ale.py
@@ -2,6 +2,6 @@ import subprocess
 
 def test_ale_small_simulation():
     try:
-        process = subprocess.run("ale config_small_test.yaml", shell=True, check=True)
+        process = subprocess.run("./ale config_small_test.yaml", shell=True, check=True)
     except:
         assert False, "ale couldn't run"
