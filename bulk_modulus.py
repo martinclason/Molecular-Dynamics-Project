fcd29c33e6ac33dc0d236898dbac0ded22030fdb
danst852 <danst852@tlvm-4-3-2.ad.liu.se>
2021-11-30 13:11:15 +0100

    Added functionality to calculate optimal lattice constant with bulk_modulus.py from atoms-object created by eos.py

--- bulk_modulus.py
+++ bulk_modulus.py
@@ -1,13 +1,13 @@
 from ase.io import read
 from ase.units import kJ
 from ase.eos import EquationOfState
-
-
-
+import math as m
 from asap3 import Trajectory
-configs = Trajectory('Ag.traj')
-#configs = read('Ag.traj@0:5')
-#configs = read('Cu.traj')
+
+
+
+configs = Trajectory('Pt.traj')
+
 
 
 # Extract volumes and energies:
@@ -16,6 +16,9 @@ volumes = [ag.get_volume() for ag in configs]
 energies = [ag.get_potential_energy() for ag in configs]
 eos = EquationOfState(volumes, energies)
 v0, e0, B = eos.fit()
-#print(B / kJ * 1.0e24, 'GPa')
-eos.plot('Ag-eos.png')
-print(B)
+print(B / kJ * 1.0e24, 'GPa')
+eos.plot('Pt-eos.png', show = False)
+a0 = m.sqrt(v0)
+#print(B)
+print("Optimal volume is: " + str(v0))
+print("Optimal lattice constant is: " + str(a0))
--- eos.py
+++ eos.py
@@ -1,18 +1,19 @@
 import numpy as np
 
-from ase import Atoms
-from ase.io.trajectory import Trajectory
-from ase.calculators.emt import EMT
+from asap3 import Atoms
+from asap3 import Trajectory
+from asap3 import EMT
 
-a = 4.0  # approximate lattice constant
+
+a = 3.52  # approximate lattice constant
 b = a / 2
-ag = Atoms('Ag',
+ag = Atoms('Pt',
            cell=[(0, b, b), (b, 0, b), (b, b, 0)],
            pbc=1,
            calculator=EMT())  # use EMT potential
 cell = ag.get_cell()
-traj = Trajectory('Ag.traj', 'w')
-for x in np.linspace(0.95, 1.05, 10):
+traj = Trajectory('Pt.traj', 'w')
+for x in np.linspace(0.95, 1.05, 1000):
     ag.set_cell(cell * x, scale_atoms=True)
     ag.get_potential_energy()
     traj.write(ag)
