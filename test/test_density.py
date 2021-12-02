2a94b677b9b44b3a6b166427b9bc7f5e11769551
danle402 <danle402@tlvm-4-1-1.ad.liu.se>
2021-12-02 14:54:49 +0100

    Implementation of real density calculation Implemented a real calculation for the materials density from .traj file and also a plot function that can plot over time.

--- test/test_density.py
+++ test/test_density.py
@@ -1,9 +1,11 @@
-from main import density
+from density import density, density_plot
+from asap3 import Trajectory
 
 import yaml
 
-# Parsing yaml config_file
+# Parsing ySaml config_file
 config_file = open("config.yaml")
+traj_read = Trajectory("test.traj")
 # Could be changed to current working directory
 #root_d = os.path.dirname(__file__)
 #config_file = open(os.path.join(root_d, "config.yaml"))
@@ -11,6 +13,6 @@ parsed_config_file = yaml.load(config_file, Loader=yaml.FullLoader)
 parsed_config_file["use_asap"] = False
 
 def test_density():
-    assert density(parsed_config_file) >= 0 #is the density a non negative number?
+    assert density(0,traj_read,parsed_config_file) >= 0 #is the density a non negative number?
 
 test_density()
