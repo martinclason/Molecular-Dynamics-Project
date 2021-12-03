bef763b996e6f22bcaf29fc43212b6fd145dd441
Gustaf Åhlgren <gusah153@student.liu.se>
2021-12-03 14:05:14 +0100

    Modified testenviroment

--- test/test_specificHeatCapacity.py
+++ test/test_specificHeatCapacity.py
@@ -1,21 +1,25 @@
 from specificHeatCapacity import specificHeatCapacity
 import sys
+import os
+
+def test_file():
+    assert os.path.isfile('test/Ar.traj')
 
 # Test if the calculated specific heat capacity of Argon is somewhat 
 # close to the real value.
-C_v_Ar = specificHeatCapacity("NVE",'Ar.traj')
+C_v_Ar = specificHeatCapacity("NVE",'test/Ar.traj')
 
 def test_specificHeatCapacity_Ar():
     assert (C_v_Ar >= 0.5) and (C_v_Ar <= 3) 
 
-C_v_Cu = specificHeatCapacity("NVE",'Cu.traj')
+C_v_Cu = specificHeatCapacity("NVE",'test/Cu.traj')
 
 def test_specificHeatCapacity_Cu():
     assert (C_v_Cu >= 0.1) and (C_v_Cu <= 0.7)
 
 # Tests if the function throws an exception when an unsupported 
 # ensamble is specified.
-def test_ensamble_exception(self):
+def test_ensamble_exception():
    try:
        specificHeatCapacity("Not an ensamble","Ar.traj")
    except:
@@ -23,7 +27,7 @@ def test_ensamble_exception(self):
 
 # Tests if the function throws an exception when an unsupported 
 # ensamble is specified.
-def test_trajectory_file_exception(self):
+def test_trajectory_file_exception():
    try:
        specificHeatCapacity("NVE","not_a_file.file")
    except:
