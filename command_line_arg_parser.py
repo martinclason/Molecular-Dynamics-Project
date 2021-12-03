d661f339f6e7cae1dbb9d93c6ece2d2743a20505
Martin Clason <martin@clason.se>
2021-12-03 22:18:20 +0100

    Adds subcommands for ale to run e.g. sim or viz
    This makes it possible to run:
    ale simulate
    ale analyze
    ale visualization
    
    If only ale is run, the default is to run both sim,
    and analysis.

--- command_line_arg_parser.py
+++ command_line_arg_parser.py
@@ -1,13 +1,30 @@
 import argparse
 
 # Adds parser so user can choose if to use asap or not with flags from terminal
-parser = argparse.ArgumentParser()
-parser.add_argument('--asap', dest='use_asap', action='store_true')
-parser.add_argument('--no-asap', dest='use_asap', action='store_false')
-parser.set_defaults(use_asap=True)
 
-parser.add_argument('config_file',
-                    nargs='?',
-                    type=argparse.FileType('r'),
-                    default='./config.yaml')
+def CreateParser(default, simulate, analyze, visualize):
+  
+  parser = argparse.ArgumentParser()
+  subparsers = parser.add_subparsers()
 
+  parser.add_argument('--asap', dest='use_asap', action='store_true')
+  parser.add_argument('--no-asap', dest='use_asap', action='store_false')
+  parser.set_defaults(use_asap=True)
+
+  parser.add_argument('config_file',
+                      nargs='?',
+                      type=argparse.FileType('r'),
+                      default='./config.yaml')
+
+  parser.set_defaults(sub_command=default)
+
+  parser_simulate = subparsers.add_parser('simulate')
+  parser_simulate.set_defaults(sub_command=simulate)
+
+  parser_analyze = subparsers.add_parser('analyze')
+  parser_analyze.set_defaults(sub_command=analyze)
+
+  parser_visualize = subparsers.add_parser('visualize')
+  parser_visualize.set_defaults(sub_command=visualize)
+
+  return parser
