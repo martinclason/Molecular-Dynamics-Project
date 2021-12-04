ac255772e029bf1a6c979cc1f9d6830b6f4ff410
Martin Clason <martin@clason.se>
2021-12-04 11:37:53 +0100

    Restructured passing of subcommands

--- command_line_arg_parser.py
+++ command_line_arg_parser.py
@@ -3,7 +3,7 @@ import argparse
 # Adds parser so user can choose if to use asap or not with flags from terminal
 
 class CreateParser():
-    def __init__(self, default, simulate, analyze, visualize):
+    def __init__(self, default, subcommands):
         self.parser = argparse.ArgumentParser()
         self.subparsers = self.parser.add_subparsers()
 
@@ -21,20 +21,13 @@ class CreateParser():
 
         self.parser.set_defaults(sub_command=default)
 
-        parser_simulate = self.subparsers.add_parser('simulate')
-        parser_simulate.set_defaults(sub_command=simulate)
-
-        parser_analyze = self.subparsers.add_parser('analyze')
-        parser_analyze.set_defaults(sub_command=analyze)
-
-        parser_visualize = self.subparsers.add_parser('visualize')
-        parser_visualize.set_defaults(sub_command=visualize)
+        for command in subcommands:
+            cmd = self.subparsers.add_parser(command)
+            cmd.set_defaults(sub_command=subcommands[command])
 
     def parse_args(self):
         # multipass strategy: 
         # https://stackoverflow.com/questions/46962065/add-top-level-argparse-arguments-after-subparser-args
         args = self.parser.parse_known_args()
-        print('pass 1: ', args)
         args = self.parser.parse_args(args[1], args[0])
-        print('pass 2:', args)
         return args
