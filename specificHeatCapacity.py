eac889570b30767e81365f87926dfd66416ebd8c
Martin Clason <martin@clason.se>
2021-12-06 20:28:14 +0100

    Corrects misspelling

--- specificHeatCapacity.py
+++ specificHeatCapacity.py
@@ -3,36 +3,36 @@ import numpy as np
 from ase import atoms, units
 import sys
 
-def specificHeatCapacity(ensamble, traj):
+def specificHeatCapacity(ensemble, traj):
   """This function calculates the specific heat capacity from the trajectory output
-  file and the ensamble that has been simulated."""
+  file and the ensemble that has been simulated."""
 
   # Calculates the number of atoms and the temperature of the simulated system.
   N = len(traj[1].get_positions()) # Number of atoms is the same as number of positions
   T = np.sum([atoms.get_temperature() for atoms in traj[-20:]])/20 # Average of last 10 itterations
 
-  if ensamble == "NVE":
+  if ensemble == "NVE":
     # Calculates the kinetic energy per atom and then proceeds to calculate the 
     # variance in the kinetic energy per atom.
     total_energies = [atoms.get_kinetic_energy() for atoms in traj]
     total_energies = [energy / N for energy in total_energies] 
     var_e = np.var(total_energies)
     
-    # Calculates the specific heat capacity when the NVE ensamble has been simulated.
+    # Calculates the specific heat capacity when the NVE ensemble has been simulated.
     C_v = ((3*N*units.kB)/2) / (1 - (2/(3*(units.kB*T)**2))*var_e)
 
-  elif ensamble == "NVT":
+  elif ensemble == "NVT":
     # Calculates the variance in the total energy per atom
     total_energies = [atoms.get_total_energy() for atoms in traj]
     total_energies = [energy / N for energy in total_energies] 
     var_e = np.var(total_energies)
 
-    # Calculates the specific heat capacity when the NVT ensamble has been simulated.
+    # Calculates the specific heat capacity when the NVT ensemble has been simulated.
     C_v = 1/(units.kB*T**2) * var_e
 
   else:
-    # Throws an exception if the specified ensamble isn't supported.
-    raise Exception("Unknown ensamble: {}".format(ensamble))
+    # Throws an exception if the specified ensemble isn't supported.
+    raise Exception("Unknown ensemble: {}".format(ensemble))
 
   # Atomic mass unit in kg
   u = 1.66e-27 #kg
