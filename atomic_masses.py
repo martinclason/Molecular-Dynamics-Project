6713711bb767c90ef879b5a6045544e6d13a6bfc
Anton Olsson <antol302@student.liu.se>
2021-12-06 10:17:07 +0100

    Added documentation to atomic_masses

--- atomic_masses.py
+++ atomic_masses.py
@@ -1,6 +1,11 @@
 from ase.data import atomic_masses, atomic_numbers
 
 def atomic_masses(atom_list) :
+    """Returns the sum of the atomic masses for the molecule/elements specified by symbol
+    in the config-file. It however does this by looking at the atoms_object and retrieving
+    one molecule from one point in the lattice, i.e. it does not look in the config_file.
+    This requires the set_tag function of Atoms() to save the size of the super cell
+    when the atoms object is created by createAtoms()"""
     all_masses = atom_list[0].get_masses() 
     size_cube = atom_list[0].get_tags()[0]**3 # Tag should contain the size of super cell
     number_of_atoms = int(len(all_masses) / size_cube) # Number of atoms per molecule
