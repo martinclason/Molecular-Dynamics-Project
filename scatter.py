7d3cf05b3e2dcbc9e0719fa7137fea45f2af842a
danle402 <danle402@tlvm-4-4-3.ad.liu.se>
2021-12-07 16:40:40 +0100

    Implemented function scatter_plot() that can read dat from .json files and make a scatter plot.

--- scatter.py
+++ scatter.py
@@ -1,13 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
+
+from simulationDataIO import inputSimulationData
 
 def scatter_plot(filelist,data_type1,data_type2):
+    """The function scatter_plot takes the arguments filelist, 
+    data_type1 and data_type2. Filelist is a list of names 
+    of the output files from analyse. data_type1 and data_type2 specifies
+    what two data_types the functions is to do a scatter plot of. The function
+    returns a scatter plot. """
+
     for file in filelist:
-        data = pd.read_csv(file)
-        x = data.data_type1
-        y = data.data_type2
-        plt.scatter(x,y,alpha=0.5)
+        data = inputSimulationData(file)
+        x = data[data_type1]
+        y = data[data_type2]
+        plt.scatter(x,y)
+        plt.xlabel(data_type1)
+        plt.ylabel(data_type2)
     plt.show()
-
-
