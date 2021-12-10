005e0af1245216544b3d57a6d9bdcc88b7791fad
Martin Clason <martin@clason.se>
2021-12-10 21:50:19 +0100

    Adds unit test for openkim

---
+++ test/test_openkim.py
@@ -0,0 +1,8 @@
+from ase.calculators.kim.kim import KIM
+
+def test_loading_openkim_potential():
+  openKIMid = "Sim_ASAP_EMT_Rasmussen_AgAuCu__SM_847706399649_000"
+  try:
+    calc = KIM(openKIMid)
+  except:
+    assert False, f"Couldn't load openKIM model with id {openKIMid}"
