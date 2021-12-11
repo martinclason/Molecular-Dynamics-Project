ef0305e0d0e35e78a0b23475c703e7e298d3d783
Martin Clason <martin@clason.se>
2021-12-11 15:19:11 +0100

    Changes format that potential is specified
    Can also specify LJ as potential to use built in when openKIM
    isn't available.

--- main.py
+++ main.py
@@ -18,6 +18,71 @@ from equilibriumCondition import equilibiriumCheck
 from ase.calculators.kim.kim import KIM
 
 from simulationDataIO import outputGenericFromTraj
+from aleErrors import ConfigError
+
+def built_in_LennardJones(options, use_asap):
+    # Fallback/default values if not present in config
+    fallback_atomic_number = 1
+    fallback_epsilon = 0.010323 # eV
+    fallback_sigma = 3.40 # Å
+    fallback_cutoff = 6.625 # Å
+
+    if use_asap:
+        print("Running LJ potential with asap")
+        from asap3 import LennardJones
+
+        atomic_number = options.get("atomic_number", fallback_atomic_number)
+        epsilon = options.get("epsilon", fallback_epsilon) * units.eV
+        sigma = options.get("sigma", fallback_sigma) * units.Ang
+        cutoff = options.get("cutoff", fallback_cutoff) * units.Ang
+        
+        keys = ("atomic_number", "epsilon", "sigma", "cutoff")
+        if not all (key in options for key in keys):
+            print(f"Warning, using fallback values for some values in: {keys}")
+
+        return LennardJones(
+                [atomic_number],
+                [epsilon],
+                [sigma],
+                rCut=cutoff,
+                modified=True,
+            )
+    else:
+        print("Running LJ potential with ase")
+        from ase.calculators.lj import LennardJones
+
+        epsilon = options.get("epsilon", fallback_epsilon) * units.eV
+        sigma = options.get("sigma", fallback_sigma) * units.Ang
+        
+        keys = ("epsilon", "sigma")
+        if not all (key in options for key in keys):
+            print(f"Warning, using fallback values for some values in: {keys}")
+
+        return LennardJones(
+                epsilon=epsilon,
+                sigma=sigma,
+            )
+
+def create_potential(options, use_asap):
+    potential_str = options["potential"]
+    if potential_str.lower() in ("lj", "LennardJones".lower()):
+        # return built in LennardJones
+        return built_in_LennardJones(options, use_asap)
+    if "openkim:" in potential_str.lower():
+        # extract openKIM id from string prefixed with 'openkim:'
+        openKIMpotential_str = potential_str.split(":")[1]
+        try:
+            return KIM(openKIMpotential_str)
+        except:
+            raise ConfigError(
+                    message=f"A openKIM potential couldn't be created from given config: {openKIMpotential_str}",
+                    config_properties=["potential"],
+                  )
+    raise ConfigError(
+                message=f"No potential could be created from given config: {potential_str}",
+                config_properties=["potential"],
+          )
+
 
 def MD(options):
     """The function 'MD()' runs defines the ASE and ASAP enviroment to run the
@@ -31,7 +96,7 @@ def MD(options):
     interval = options["interval"] if options["interval"] else 10
 
     if use_asap:
-        print("Running with asap")
+        print("Running dynamics with asap")
         from asap3.md.verlet import VelocityVerlet
     else:
         print("Running with ase")
@@ -39,7 +104,9 @@ def MD(options):
 
     # Set up a crystal
     atoms = createAtoms(options)
-    atoms.calc = KIM(options["openKIMid"]) # Attach the openKIM calculator
+    calc = create_potential(options, use_asap)
+    print(f"Using potential: {calc}")
+    atoms.calc = calc
     
     time_step = options["dt"] * units.fs
     temperature = options["temperature_K"]
