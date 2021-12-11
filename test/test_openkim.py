18818a7c1c87015a2b1154365483581888a2854e
Martin Clason <martin@clason.se>
2021-12-11 11:28:41 +0100

    Adds test for default openKIM pot

--- test/test_openkim.py
+++ test/test_openkim.py
@@ -1,9 +1,17 @@
 from ase.calculators.kim.kim import KIM
 
+
+def test_loading_universal_openkim_potential():
+  try:
+    openKIMid = "LJ_ElliottAkerson_2015_Universal__MO_959249795837_003"
+    calc = KIM(openKIMid)
+  except:
+    assert False, f"Couldn't load universal openKIM model with id {openKIMid}"
+
 def test_loading_openkim_potential():
   try:
     openKIMid = "Sim_ASAP_EMT_Rasmussen_AgAuCu__SM_847706399649_000"
-    # calc = KIM(openKIMid)
+    calc = KIM(openKIMid)
   except:
     assert False, f"Couldn't load openKIM model with id {openKIMid}"
 
