7988798b169f9d0e229862398106424d12c56412
Martin Clason <martin@clason.se>
2021-12-12 23:25:01 +0100

    Experimenting with parallel processes and pickles of options list

---
+++ test/test_parallel.py
@@ -0,0 +1,15 @@
+from pickle_options import pickle_options
+from subprocess import Popen, PIPE
+import os
+
+
+
+def test_parallel_proof_of_concept():
+    pickle_options()
+
+    parallel_script = 'parallel_execution_trial.py'
+
+    assert os.path.isfile('options_pickle')
+
+    process = Popen(['mpirun', 'python3', parallel_script])
+
