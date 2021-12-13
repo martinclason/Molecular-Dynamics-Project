a82ea6422841c9e86365477c48a476f3edd33bae
danle402 <danle402@tlvm-4-3-1.ad.liu.se>
2021-12-13 15:46:23 +0100

    Fixed comments from PR

--- scatter.py
+++ scatter.py
@@ -12,54 +12,62 @@ from simulationDataIO import inputSimulationData
 def find_json_files(options):
     file_list = []
     outdir = options.get("scatter_dir") if options.get("scatter_dir") is not None else os.getcwd() 
-    try:
-        if os.path.isdir(os.path.join(os.getcwd(),outdir)):
-            for file in glob.glob(os.path.join(os.getcwd(),outdir,"*.json")):
-                file_list.append(file)
-                print(file_list)
-            return file_list
-        else:
-            raise ConfigError
-    except ConfigError(outdir,"Does not exist"): 
-        print(ERRROOOORRR)
-    #if os.path.isdir(os.path.join(os.getcwd(),outdir)): #Checks if chosen directory exists
-     #   for file in glob.glob(os.path.join(os.getcwd(),outdir,"*.json")):
-      #      file_list.append(file)
-       #     print(file_list)
-        #if file_list == []:
-         #   print("There where no .json files in the chosen diectory" + os.getcwd() + "/" + outdir + ".")
-        #return file_list
-    #else:
-     #  print("The chosen directory " + os.getcwd() + "/" + outdir + " does not exist.")
+    
+    if os.path.isdir(os.path.join(os.getcwd(),outdir)):
+        for file in glob.glob(os.path.join(os.getcwd(),outdir,"*.json")):
+            file_list.append(file)
+            print(file_list)
+        return file_list
+    else:
+        raise ConfigError(
+                message= "Chosen directory path " + os.path.join(os.getcwd(),outdir) + " doesnt exist.",
+                config_properties = ["scatter_dir"]
+    )
 
 
 def make_scatter_plotter(options,data_type1,data_type2,filelist=[]):
-    """The function scatter_plot takes the arguments filelist, 
-    data_type1 and data_type2. Filelist is a list of names 
-    of the output files from analyse. data_type1 and data_type2 specifies
-    what two data_types the functions is to do a scatter plot of. The function
-    returns a scatter plot. """
+    """Creates and returns a plotter function that creates a scatter plot
+    
+    :param options: parsed options from config file.
+    :param data_type1: data type to write on the x-axis.
+    :param data_type2: data type to write on the y-axis.
+    :param filelist: list of .json files, if specification of which files to do plotter of is needed, default=[]."""
     filelist = options.get("scatter_files") if options.get("scatter_files") != [] else find_json_files(options) 
     def scatter_plotter():
         if filelist is None:
-            print("ERROR: Scatterplot could not be created!")
+            raise ConfigError(
+                    message = "List of files is of type None",
+                    config_properties = ["scatter_files"]
+        )
         else:
             fig = plt.figure()
             ax = plt.axes()
             for file in filelist:
-                #if not os.path.isfile(file): #Checks if file in filelist exists in chosen directory
-                    #print("File " + file + " does not exist.")
-                #else:
                 try:
                     data = inputSimulationData(file)
                 except OSError:
-                    print("Thats no file")
-                    sys.exit()  
+                    raise ConfigError(
+                            message = "Filename " + file + " does not exist.",
+                            config_properties = ["scatter_files"]
+                ) 
                 if data_type1 == "time":
                     dt = 2
                     x = np.arange(0, len(data[data_type2])*dt, dt)
                 else:
-                    x = data[data_type1]                    
+                    try:
+                        x = data[data_type1]
+                    except KeyError:
+                        raise ConfigError(
+                                message = "Must specify which data type to plot on the x-axis.",
+                                config_properties = ["data_type1"]
+                    )
+                try:
+                    y = data[data_type2]
+                except KeyError:
+                    raise ConfigError(
+                            message = "Must specify which data type to plot on the y-axis.",
+                            config_properties = ["data_type2"]
+                )
                 y = data[data_type2]
                 ax.scatter(x,y)
                 plt.xlabel(data_type1)
