56a0d9b32066eaa326ff4177903736b8331866ff
Martin Clason <martin@clason.se>
2022-01-08 18:10:04 +0100

    Adds docs

--- ale/errors.py
+++ ale/errors.py
@@ -1,6 +1,12 @@
 
 class ConfigError(Exception):
-  """Used to raise errors when issues with the configuration file is encountered"""
+  """Used to raise errors when issues with the configuration file is encountered
+
+  :param config_properties: properties involved in error
+  :type config_properties: list of str
+  :param message: message describing the error
+  :type message: str
+  """
   def __init__(self, config_properties, message):
     self.message = message
     self.config_properties = config_properties
@@ -8,4 +14,4 @@ class ConfigError(Exception):
   def __str__(self):
     return f"""{self.message}
 Involved config properties: {", ".join(self.config_properties)}"""
-    
+
