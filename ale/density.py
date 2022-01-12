6e4b2ea10a1a49f973e75c3ce222a96b235ccd7e
Martin Clason <martin@clason.se>
2022-01-12 13:53:02 +0100

    Fix typo

--- ale/density.py
+++ ale/density.py
@@ -3,8 +3,8 @@ import math
 
 
 def density(atoms_object):
-    """The function 'density()' takes a time t, a list of atoms from .traj file 
-    and config options as argument and calculates the density of the chosen 
+    """The function 'density()' takes a time t, a list of atoms from .traj file
+    and config options as argument and calculates the density of the chosen
     time step t. Prints and returns density in g/cm^3"""
     m = sum(atoms_object.get_masses()) * (1.6605387E-24)
     V = atoms_object.cell.volume*10**(-24)
@@ -14,7 +14,7 @@ def density(atoms_object):
 
 # TODO: Should we keep density plot? Might need some restructuring since density() now takes atoms_object instead of atom_list
 def density_plot(time,atom_list,options):
-    """The function 'density_plot()' takes a amount of timesteps, a list of atoms
+    """The function 'density_plot()' takes an amount of timesteps, a list of atoms
     from a .traj file amd options from config file and plots the density over time.
     This can be used if the Volume is not constant over time."""
     density_data=[]
